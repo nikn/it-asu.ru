84c924a4afa7ecb3d67f5996388c3afe2aabc457
dardvas <dardvas@gmail.com>
2016-04-09 03:42:12 +0600

    news, article models

---
+++ backend/views/site/addNews.php
@@ -0,0 +1,31 @@
+<?php
+
+/* @var $this yii\web\View */
+
+$this->title = 'Добавить новость';
+?>
+<div class="site-index">
+
+    <div class="body-content">
+
+        <div class="row">
+            <div class="col-lg-12">
+                <h2>Страница добавления новости</h2>
+            </div>
+        </div>
+        
+        <div class="row">
+            <div class="col-lg-12">
+                <form method="post">
+                    Заголовок статьи: <input type="text"><br>
+                    Картинка превью: <input type="file"><br>
+                    Текст анонса: <input type="text" height="5"><br>
+                    Подробный текст (HTML) <input type="text" height="20"><br>
+                    <input type="submit" value="Создать новость">
+                </form>
+            </div>
+        </div>
+        
+    </div>
+    
+</div>
---
+++ common/models/Article.php
@@ -0,0 +1,70 @@
+<?php
+namespace common\models;
+
+use Yii;
+use yii\base\NotSupportedException;
+use yii\behaviors\TimestampBehavior;
+use yii\db\ActiveRecord;
+use yii\web\IdentityInterface;
+
+/**
+ * Article model
+ * @property int $id
+ * @property string $title
+ * @property string $preview_text
+ * @property string $preview_img_url
+ * @property string $detail_text
+ * @property string $detail_link
+ */
+class Article extends ActiveRecord implements IdentityInterface
+{
+    const STATUS_DELETED = 0;
+    const STATUS_ACTIVE = 10;
+
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return '{{%article}}';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function behaviors()
+    {
+        return [
+            TimestampBehavior::className(),
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            ['status', 'default', 'value' => self::STATUS_ACTIVE],
+            ['status', 'in', 'range' => [self::STATUS_ACTIVE, self::STATUS_DELETED]],
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public static function findIdentity($id)
+    {
+        return static::findOne(['id' => $id, 'status' => self::STATUS_ACTIVE]);
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function getId()
+    {
+        return $this->getPrimaryKey();
+    }
+    
+
+}
